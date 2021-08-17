9ffb0de415f8aee6058a951eaf64678fc6bec63a
Cloud User <ec2-user@ip-172-31-60-133.ec2.internal>
2021-08-17 18:44:30 +0000

    #my first commit

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# welcome massage
+echo Welcome to LUCKY DIEU
+echo please Enter your username
+read $username
+echo the name you enter is $username
+echo please enter your password
+ read -s password
+echo please confirm your password
+read -s  password
